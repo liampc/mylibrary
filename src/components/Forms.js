730d433b8557807810e5ba255a31e4574c031c9c
Kathlia Carlos <lia.carlos23@gmail.com>
2020-08-07 16:50:03 +0800

    add Forms comp

---
+++ src/components/Forms.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Forms = () => {
+    return (
+       <form>
+            <input type="text" name="add-title" id="input-title" placeholder="Title" />
+            <input type="text" name="add-author" id="input-author" placeholder="Author" />
+            <select id="status">
+                <option value="To-Read">To Read</option>
+                <option value="Read">Read</option>
+            </select>
+       </form>
+    )
+}
+
+export default Forms
