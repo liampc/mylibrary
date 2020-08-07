a59458ce1d3fd497cb7cd693cd6d954d3009efae
Kathlia Carlos <lia.carlos23@gmail.com>
2020-08-07 15:47:31 +0800

    add temp header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
 
 
 const App = () => {
     return(
-        <h1>Hello</h1>
+        <div>
+            <Header />
+        </div>
+        
     )
 }
 
