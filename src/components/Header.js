5c03fbf60a40907114a3898632c6607e5634169f
Kathlia Carlos <lia.carlos23@gmail.com>
2020-08-07 17:00:07 +0800

    add Header comp

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div>
+           <span>Title</span>
+           <span>Author</span>
+           <span>Status</span>
+           <span></span>
+        </div>
+       
+        
+    )
+}
+
+export default Header
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const Lists = () => {
     return(
         <div>
-            {/* <Subhead /> */}
+            <Header />
             {/* <Table /> */}
         </div>
        
