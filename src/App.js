f9ce3564064565dafeeb87ac749d6f1bece86581
Kathlia Carlos <lia.carlos23@gmail.com>
2020-08-09 16:09:16 +0800

    add css for banner and in app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from './components/Banner'
 import Content from './components/Content'
+import './App.css'
 
 
 const App = () => {
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import library from "./assets/library.jpg"
 
 const Banner = () => {
+    const style = {
+        backgroundImage: `url(${library})`
+    }
+
     return(
-        <header>
+        <header style={style}>
             <h1>My Library</h1>
         </header>
         
